45cd935053426587f8a66a092e3cc20d7578347c
yuki-js <me@aoki.app>
2026-07-06 19:39:55 +0000

    referenceを削除

--- reference/db/migration/V3__Add_meta_to_friendships.sql
+++
@@ -1,6 +0,0 @@
--- Add meta column to friendships table
--- This allows storing flexible metadata for each friendship exchange
-
-ALTER TABLE friendships ADD COLUMN meta JSONB;
-
-COMMENT ON COLUMN friendships.meta IS 'Flexible metadata captured during profile card exchange';
